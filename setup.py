bbabbca9e7e0d819ccad52b41b8ebf497e4d0f5d
Sidd Lokray <62676286+siddlokray@users.noreply.github.com>
2025-06-14 20:33:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
-    name="cobra",
+    name="cobra-brain",
     version="0.1.0",
     author="Sidd Lokray",
     author_email="siddharthlokray@gmail.com",
