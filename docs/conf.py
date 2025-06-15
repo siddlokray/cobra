96796043eb6d2de926da8db8c8245230cae84cc4
Sidd Lokray <62676286+siddlokray@users.noreply.github.com>
2025-06-15 08:30:34 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 # Project information
-project = 'CoBrA'
+project = 'CoBRA'
 copyright = '2025, Siddharth Lokray'
 author = 'Siddharth Lokray'
 release = '0.1.2'
